168d2f9c434ab2b09d94b17505401e8fd2b465af
guillaume collet <guillaume.collet@ac-rennes.fr>
2023-08-24 09:45:09 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom'
+import './App.css'
+import { HomePage } from './layouts/HomePage/HomePage'
+import { Footer } from './layouts/NavbarAndFooter/Footer'
+import { Navbar } from './layouts/NavbarAndFooter/NavBar'
+import { SearchBookPage } from './layouts/SearchBookPage/SearchBookPage'
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage'
+import { oktaConfig } from './lib/oktaConfig'
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
+import { Security, LoginCallback } from '@okta/okta-react'
+import LoginWidget from './Auth/LoginWidget'
 
-function App() {
+const oktaAuth = new OktaAuth(oktaConfig)
+
+export const App = () => {
+  const history = useHistory()
+
+  const customAuthHandler = () => {
+    history.push('/login')
+  }
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='d-flex flex-column min-vh-100'>
+      <Security
+        oktaAuth={oktaAuth}
+        restoreOriginalUri={restoreOriginalUri}
+        onAuthRequired={customAuthHandler}
+      >
+        <Navbar />
+        <div className='flex-grow-1'>
+          <Switch>
+            <Route
+              path='/'
+              exact
+            >
+              <Redirect to='/home' />
+            </Route>
+            <Route path='/home'>
+              <HomePage />
+            </Route>
+            <Route path='/search'>
+              <SearchBookPage />
+            </Route>
+            <Route path='/checkout/:bookId'>
+              <BookCheckoutPage />
+            </Route>
+            <Route
+              path='/login'
+              render={() => <LoginWidget config={oktaConfig} />}
+            />
+            <Route
+              path='/login/callback'
+              component={LoginCallback}
+            />
+          </Switch>
+        </div>
+        <Footer />
+      </Security>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/layouts/NavbarAndFooter/NavBar.tsx
@@ -0,0 +1,79 @@
+import { Link, NavLink } from 'react-router-dom'
+import { useOktaAuth } from '@okta/okta-react'
+import { SpinnerLoading } from '../Utils/SpinnerLoading'
+
+export const Navbar = () => {
+  const { oktaAuth, authState } = useOktaAuth()
+
+  if (!authState) {
+    return <SpinnerLoading />
+  }
+
+  const handleLogout = async () => oktaAuth.signOut()
+
+  console.log(authState)
+
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+      <div className='container-fluid'>
+        <span className='navbar-brand'>Luv 2 read</span>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNavDropDown'
+          aria-controls='navbarNavDropDown'
+          aria-expanded='false'
+          aria-label='Toggle Navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div
+          className='collapse navbar-collapse'
+          id='navbarNavDropDown'
+        >
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <NavLink
+                to='/home'
+                className='nav-link'
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink
+                to='/search'
+                className='nav-link'
+              >
+                Search books
+              </NavLink>
+            </li>
+          </ul>
+          <ul className='navbar-nav ms-auto'>
+            {!authState.isAuthenticated ? (
+              <li className='nav-item m-1'>
+                <Link
+                  type='button'
+                  className='btn btn-outline-light'
+                  to='/login'
+                >
+                  Signin
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <button
+                  className='btn btn-outline-light'
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
